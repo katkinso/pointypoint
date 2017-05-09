4cf418f3b51712d912583f44e83ad3c4999aea14
katkinso <kate.atkinson@neustar.biz>
2017-05-09 12:25:43 -0700

    env

--- controllers/slack-controller.js
+++ controllers/slack-controller.js
@@ -3,14 +3,12 @@ var bodyParser = require('body-parser')
 var Promise = require('bluebird')
 var request = Promise.promisifyAll(require("request"), {multiArgs: true})
 
-// process.env['SLACK_TOKEN'] = "0I7TFXDQawvFZC7uW4l4zxZR"
-var slack_token = process.env['SLACK_TOKEN'] // '42348901293989849243'
-console.log('slacktoken= ' + slack_token)
+var slack_token = process.env['SLACK_TOKEN']
+// console.log('slacktoken= ' + slack_token)
 
 var slackController = function(app,io){
 
     var urlencodedParser = bodyParser.urlencoded({extended:false})
-
 
     app.get('/',function(req,res){
         res.render('index')
@@ -20,7 +18,6 @@ var slackController = function(app,io){
     app.post('/',urlencodedParser,function(req,res){
 
         if (req.body.token == slack_token){
-
 
           var message = {
             'points': req.body.text,

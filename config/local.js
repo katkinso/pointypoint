2ede89611eee17cc8f626865e850248c2ce5c856
katkinso <kate.atkinson@neustar.biz>
2017-05-10 14:08:25 -0700

    test

--- config/local.js
+++
@@ -1,6 +0,0 @@
-// process.env['SLACK_TOKEN'] = "0I7TFXDQawvFZC7uW4l4zxZR"
-// https://hooks.slack.com/services/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
-
-
-process.env['SLACK_POST_TOKEN'] = "2GOft9t2hQ5Pley7eeM6Es7w";
-process.env['SLACK_VERIFICATION_TOKEN'] = "0I7TFXDQawvFZC7uW4l4zxZR";

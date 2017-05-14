ad8cae2641909bbaf560892327608d0b4a2d97f0
katkinso <kate.atkinson@neustar.biz>
2017-05-13 17:13:50 -0700

    chart

--- public/slack.js
+++ public/slack.js
@@ -1,18 +1,34 @@
 $(document).ready(function(){
 
-    //instatntiate socket.io
-    var socket = io();
+      //instatntiate socket.io
+      var socket = io();
 
-    //render voting messages onto screen
-    socket.on("message", function(message) {
 
-      var p = $('<p />')
-      p.append(message.userName + ' | ' + message.points)
-      $('div#'+message.uuid).append(p)
 
-      console.log("message: ", message)
-      
-    });
+
+      //render voting messages onto screen
+      socket.on("message", function(message) {
+          //append voting msg to page
+          var p = $('<p />')
+          p.append(message.userName + ' | ' + message.points)
+          $('div#'+message.uuid).append(p)
+
+          var ctx = $('#'+uuid);
+          var myChart = new Chart(ctx, buildChart(message.userName,message.points));
+          // console.log(buildChart(labels,points))
+
+      });
+
+      //temp
+      // var uuid = 123
+      // var ctx = $('#'+uuid);
+      // var labels = 'kate'
+      // var points = 5
+      //
+      //
+
+
+
 
 
   $('form#addTask').on('submit', function(){
@@ -28,6 +44,7 @@ $(document).ready(function(){
         data: {'task_name':taskName,'num_people':numPeople,'uuid':uuid},
         success: function(data){
           //do something with the data via front-end framework
+
           var div = $('<div />')
           var h3 = $('<h3>')
 
@@ -35,6 +52,13 @@ $(document).ready(function(){
           h3.append(taskName)
           div.append(h3)
           $('#main').append(div)
+
+          //add chart
+          var canvas = $("<canvas />")
+          canvas.attr('id',uuid)
+          canvas.attr('width',300)
+          canvas.attr('height',300)
+          $('#main').append(canvas)
 
 
           // location.reload();

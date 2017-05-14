c8c9723b5a58eed4006ebdf5a480722b3f1c0e27
katkinso <kate.atkinson@neustar.biz>
2017-05-13 22:12:44 -0700

    ss

--- public/slack.js
+++ public/slack.js
@@ -49,6 +49,62 @@ $(document).ready(function(){
           div.append(h3)
           $('#main').append(div)
 
+        //   var ctx = $('#myChart')
+        //   var myChart = new Chart(ctx,
+        //
+        //   {
+        //      "type":"bar",
+        //      "data":{
+        //         "labels":[
+        //            "katyratkinson1",
+        //            "katyratkinson0"
+        //         ],
+        //         "datasets":[
+        //            {
+        //               "label":"# of Votes",
+        //               "data":[
+        //                  "3",
+        //                  "4"
+        //               ],
+        //               "backgroundColor":[
+        //
+        //                     "rgba(255, 99, 132, 0.2)",
+        //                     "rgba(54, 162, 235, 0.2)",
+        //                     "rgba(255, 206, 86, 0.2)",
+        //                     "rgba(75, 192, 192, 0.2)",
+        //                     "rgba(153, 102, 255, 0.2)",
+        //                     "rgba(255, 159, 64, 0.2)"
+        //
+        //               ],
+        //               "borderColor":[
+        //
+        //                     "rgba(255,99,132,1)",
+        //                     "rgba(54, 162, 235, 1)",
+        //                     "rgba(255, 206, 86, 1)",
+        //                     "rgba(75, 192, 192, 1)",
+        //                     "rgba(153, 102, 255, 1)",
+        //                     "rgba(255, 159, 64, 1)"
+        //
+        //               ],
+        //               "borderWidth":1
+        //            }
+        //         ]
+        //      },
+        //      "options":{
+        //         "responsive":false,
+        //         "scales":{
+        //            "yAxes":[
+        //               {
+        //                  "ticks":{
+        //                     "beginAtZero":true
+        //                  }
+        //               }
+        //            ]
+        //         }
+        //      }
+        //   }
+        // )//chart
+
           // location.reload();
         }
       });

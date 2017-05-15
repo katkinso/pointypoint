8101c5e285307661c3475d46de96580d78f3e2f3
katkinso <kate.atkinson@neustar.biz>
2017-05-15 11:55:51 -0700

    colors

--- utils/utils.js
+++ utils/utils.js
@@ -19,19 +19,19 @@ function generateColors(colorsNeeded){
     var backgroundColorSet = [
     'rgba(255, 99, 132, 0.2)',
     'rgba(54, 162, 235, 0.2)'
-    // 'rgba(255, 206, 86, 0.2)',
-    // 'rgba(75, 192, 192, 0.2)',
-    // 'rgba(153, 102, 255, 0.2)',
-    // 'rgba(255, 159, 64, 0.2)'
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)'
    ]
 
     var borderColorSet = [
       'rgba(255,99,132,1)',
       'rgba(54, 162, 235, 1)'
-      // 'rgba(255, 206, 86, 1)',
-      // 'rgba(75, 192, 192, 1)',
-      // 'rgba(153, 102, 255, 1)',
-      // 'rgba(255, 159, 64, 1)'
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)'
     ]
 
 
@@ -54,9 +54,9 @@ function generateColors(colorsNeeded){
           backgroundColors = backgroundColorSet.slice(0, i)
           borderColors = backgroundColorSet.slice(0, i)
 
-          if (backgroundColors.length == i){
-            i=1
-          }
+          // if (backgroundColors.length >= i){
+          //   i=1
+          // }
 
           console.log('XXXXXXXXXXX i= ' + i)
           console.log('backgroundColors= ' + backgroundColors)
